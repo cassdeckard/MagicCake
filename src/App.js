32a522f14ed8c3dadcb3d89de33a159efaf7cede
Cass Deckard <cassdeckard@auctiontechnologygroup.com>
2025-06-18 12:40:56 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { setupEngine } from "@cassdeckard/ebbg";
+import { useEffect, useState, useRef } from "react";
+
+function Canvas() {
+  const [bgEngineState, setBgEngineState] = useState({});
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      setupEngine(canvasRef.current, bgEngineState);
+    }
+  }, [canvasRef, bgEngineState]);
+
+  return (
+    <canvas ref={canvasRef} id="canvas" width="100%" height="100%"></canvas>
+  );
+}
+export default function App() {
+  return (<Canvas />);
 }
