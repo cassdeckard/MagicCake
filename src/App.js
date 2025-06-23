e1ce3f486c3b72ee3abe3317156cb13580368ab8
Cass Deckard <cassdeckard@gmail.com>
2025-06-22 20:23:38 -0700

    Hide GitHub corner image along with UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState, useCallback } from "react";
+
 import Canvas from "./components/Canvas";
 import GithubCorner from "react-github-corner";
+
 export default function App() {
+  var [hideHud, setHideHud] = useState(false);
+
+  const toggleHideHud = useCallback(() => {
+    setHideHud(!hideHud);
+  }, [hideHud, setHideHud]);
+
   return (
     <>
-      <Canvas />
-      <GithubCorner href="https://github.com/cassdeckard/MagicCake" size={160} direction="left" />
+      <Canvas hideHud={hideHud} toggleHideHud={toggleHideHud}/>
+      <GithubCorner href="https://github.com/cassdeckard/MagicCake" size={160} direction="left" hidden={hideHud} />
     </>
   );
 }
